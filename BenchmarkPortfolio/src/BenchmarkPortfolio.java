b96e5f87c260b8fe2af1e45ae7bb74160074f26d
shweta1609 <shwetaa.singhal@gmail.com>
2018-11-25 19:35:03 -0500

    base testcases working, but need a lot of corner case handling

--- BenchmarkPortfolio/src/BenchmarkPortfolio.java
+++ BenchmarkPortfolio/src/BenchmarkPortfolio.java
@@ -8,8 +8,9 @@ import java.util.regex.Pattern;
 
 public class BenchmarkPortfolio {
 
-    private static List<List<String>> portfolio;
-    private static List<List<String>> benchmark;
+    private static List<List<String>> portfolio_list;
+    private static List<List<String>> benchmark_list;
+    private static List<Transaction> transaction_list;
 
 
     public static void main(String[] args) throws IOException {
@@ -18,23 +19,87 @@ public class BenchmarkPortfolio {
         String line;
         portfolio_list = new ArrayList<>();
         benchmark_list = new ArrayList<>();
-        Map<String, Double> asset_val;
-        Map<String, Double> bm_val;
-        List<Asset> portfolio;
-        List<Asset> benchmark;
+        List<Asset> portfolio = new ArrayList<>();
+        List<Asset> benchmark = new ArrayList<>();
+        double portfolio_total = 0;
+        double benchmark_total = 0;
         while ((line = in.readLine()) != null) {
             String[] input = line.split(Pattern.quote(":"));
             portfolio_list = getList(input[0]);
 //            System.out.println(portfolio);
             benchmark_list = getList(input[1]);
 //            System.out.println(benchmark);
-//            for(int i =0; i< portfolio.size(); i++){
-//                asset = new Asset(portfolio.get(i));
-//
-//            }
 
+            for (List<String> list: portfolio_list){
+                Asset asset = new Asset(list);
+                portfolio.add(asset);
+                portfolio_total = portfolio_total + asset.getAssetValue();
+            }
+//            System.out.println(portfolio_list);
 
+            for (List<String> list: benchmark_list){
+                Asset asset = new Asset(list);
+                benchmark.add(asset);
+                benchmark_total = benchmark_total + asset.getAssetValue();
+            }
+//            System.out.println(benchmark_list);
+            portfolio.sort(Comparator.comparing(Asset::getAssetName));
+            benchmark.sort(Comparator.comparing(Asset::getAssetName));
+            Iterator<Asset> port = portfolio.iterator();
+            Iterator<Asset> bench = benchmark.iterator();
+            Transaction trans;
+            transaction_list = new ArrayList<>();
 
+            while (port.hasNext() && bench.hasNext()) {
+                double diff = 0;
+                Asset p = port.next();
+                Asset b = bench.next();
+                if (p.getAssetName().equals(b.getAssetName())) {
+                    p.calculateAssetPercentage(portfolio_total);
+                    b.calculateAssetPercentage(benchmark_total);
+                    if (!p.equals(b)) {
+                        diff = p.getAssetValue() - b.getAssetValue();
+//                        System.out.println(diff);
+                        if(p.getAsset_type().equals("STOCK")) {
+                            trans = new Transaction();
+                            getTransaction(trans, diff, p);
+                            transaction_list.add(trans);
+                        }
+                        else if (p.getAsset_type().equals("BOND")){
+                            trans = new Transaction();
+                            diff = (p.getNumShares()*p.getPrice())-(b.getNumShares()*p.getPrice()) ;
+                            getTransaction(trans, diff, p);
+                            transaction_list.add(trans);
+                        }
+                    }
+
+                }
+
+                else if (!p.getAssetName().equals(b.getAssetName())){
+//                    Asset is not in benchmark, so selling the number of shares
+                    trans = new Transaction();
+                    diff  = p.getNumShares();
+                    getTransaction(trans, diff, p);
+                    transaction_list.add(trans);
+                }
+            }
+            transaction_list.sort(Comparator.comparing(Transaction::getAsset_name));
+            for (Transaction result: transaction_list) {
+                result.printTransaction();
+            }
+            transaction_list = null;
+        }
+    }
+
+    private static void getTransaction(Transaction trans, double diff, Asset p) {
+        if (diff < 0) {
+            trans.setSide("BUY");
+            trans.setAsset_name(p.getAssetName());
+            trans.setNum_shares(trans.calculateNumShares(diff, p.getPrice()));
+        } else if (diff > 0) {
+            trans.setSide("SELL");
+            trans.setAsset_name(p.getAssetName());
+            trans.setNum_shares(trans.calculateNumShares(diff, p.getPrice()));
         }
     }
 
@@ -57,15 +122,63 @@ public class BenchmarkPortfolio {
 
 }
 
+class Transaction{
+    private String side;
+    private String asset_name;
+    private int num_shares;
+
+    public Transaction(){
+        side = "";
+        asset_name = "";
+        num_shares = 0;
+
+    }
+
+    public double getNum_shares() {
+        return num_shares;
+    }
+
+    public String getAsset_name() {
+        return asset_name;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public void setAsset_name(String asset_name) {
+        this.asset_name = asset_name;
+    }
+
+    public void setNum_shares(double num_shares) {
+        this.num_shares = (int) num_shares;
+    }
+
+    public double calculateNumShares(double difference, double price){
+        double num;
+        num = Math.abs(difference)/price;
+        return num;
+    }
+
+    public void printTransaction(){
+        System.out.println(side+","+asset_name+","+num_shares);
+    }
+
+}
+
 class Asset{
 
-    String asset_name;
-    String asset_type;
-    float num_shares;
-    float price;
-    float accrued_interest;
-    double asset_val;
-    double asset_percent;
+    private String asset_name;
+    private String asset_type;
+    private float num_shares;
+    private float price;
+    private float accrued_interest;
+    private double asset_val;
+    private double asset_percent;
 
     public Asset(){}
 
@@ -78,18 +191,17 @@ class Asset{
     }
 
     public double getAssetValue(){
-        if (this.asset_type == "STOCK"){
+        if (asset_type.equals("STOCK")){
             this.asset_val = this.num_shares * this.price;
         }
-        else if (this.asset_type == "BOND"){
+        else if (asset_type.equals("BOND")){
             this.asset_val = this.num_shares * (this.price + this.accrued_interest) * 0.01;
         }
         return this.asset_val;
     }
 
-    public double getAssetPercentage(double total_val){
+    public void calculateAssetPercentage(double total_val){
         this.asset_percent = this.asset_val/total_val;
-        return this.asset_percent;
     }
 
     @Override
@@ -106,34 +218,30 @@ class Asset{
         return Objects.hash(asset_name, asset_percent);
     }
 
+    public String getAssetName() {
+        return asset_name;
+    }
 
-    //    private static Map<String, List<Double>> getMktVal(List<List<String>> portfolio){
-//        Map<String, Double> mkt_val = new HashMap<>();
-//        double total_val = 0;
-//        for(int i =0; i< portfolio.size(); i++){
-//            List<String> asset;
-//            List<Double> values;
-//            asset = portfolio.get(i);
-//            String name = asset.get(0);
-//            String type = asset.get(1);
-//            float shares = Float.parseFloat(asset.get(2));
-//            float price = Float.parseFloat(asset.get(3));
-//            float acc_int = Float.parseFloat(asset.get(4));
-//            double val = 0;
-//            System.out.println(name + ","  + type + "," + shares + "," + price + "," + acc_int);
-//            if (type == "STOCK"){
-//                val = shares*price;
-//            }
-//            else if (type == "BOND"){
-//                val = shares * (price + acc_int) * 0.01;
-//            }
-//
-////            mkt_val.put(name,val);
-//            total_val = total_val + val;
-//
-//        }
-//        return mkt_val;
-//    }
+    public float getNumShares() {
+        return num_shares;
+    }
+
+    public double getAsset_percent() {
+//        System.out.println(asset_percent);
+        return asset_percent;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getAsset_type(){
+        return asset_type;
+    }
+
+    public float getAccrued_interest() {
+        return accrued_interest;
+    }
 
 
 }
